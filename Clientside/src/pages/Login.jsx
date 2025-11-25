86b1ba50d543723146f0775c5f071363f870a244
Pranam.K.M <pranpranam@gmail.com>
2025-11-25 20:24:00 +0530

    Update Login.jsx

--- Clientside/src/pages/Login.jsx
+++ Clientside/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://mern-blog-wlze.onrender.com//api/auth/login', form);
+      const res = await axios.post('https://mern-blog-wlze.onrender.com/api/auth/login', form);
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       navigate('/');
