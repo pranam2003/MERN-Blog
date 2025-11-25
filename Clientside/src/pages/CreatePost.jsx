971aa66c4a085b85aad61d63fbc62202fbe77ff5
Pranam.K.M <pranpranam@gmail.com>
2025-11-25 20:13:39 +0530

    Update CreatePost.jsx

--- Clientside/src/pages/CreatePost.jsx
+++ Clientside/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const handleSubmit = async (e) => {
   }
 
   try {
-    await axios.post('http://localhost:5000/api/posts', {
+    await axios.post('https://mern-blog-wlze.onrender.com//api/posts', {
       ...post,
       tags: post.tags.split(',').map(t => t.trim())
     }, {
